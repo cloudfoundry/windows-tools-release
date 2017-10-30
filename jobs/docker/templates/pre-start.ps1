2a8a52ff4b97f653c112e27aa4d50d482420bb3d
Mark DeLillo <mdelillo@pivotal.io>
2017-10-30 16:34:25 -0400

    add docker job and package
    Signed-off-by: Sunjay Bhatia <sbhatia@pivotal.io>

---
+++ jobs/docker/templates/pre-start.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+$env:path += ";C:\var\vcap\packages\docker\docker\"
+$existingMachinePath = [Environment]::GetEnvironmentVariable("Path",[System.EnvironmentVariableTarget]::Machine)
+[Environment]::SetEnvironmentVariable("Path", $existingMachinePath + ";C:\var\vcap\packages\docker\docker", [EnvironmentVariableTarget]::Machine)
+
+dockerd --register-service
+Start-Service Docker
+
