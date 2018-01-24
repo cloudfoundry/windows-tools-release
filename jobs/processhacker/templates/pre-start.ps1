079edaeeb97894ad32792ffb0c09cf5e3c846b90
Sam Smith <sesmith177@gmail.com>
2018-01-24 17:01:43 -0500

    Add processhacker job
    [#148764505]
    
    Signed-off-by: Josh Zarrabi <jzarrabi@pivotal.io>

---
+++ jobs/processhacker/templates/pre-start.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+$mtx = New-Object System.Threading.Mutex($false, "PathMutex")
+
+if (!$mtx.WaitOne(5000)) {
+  throw "Could not acquire PATH mutex"
+}
+
+$processHackerDir='C:\var\vcap\packages\processhacker\x64'
+
+$OldPath=(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path
+$NewPath=$OldPath+';'+$processHackerDir
+Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
+
+$mtx.ReleaseMutex()
