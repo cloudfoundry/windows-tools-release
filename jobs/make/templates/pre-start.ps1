17d3328ef8bde021edf145658576d06dc8450229
Sunjay Bhatia <sbhatia@pivotal.io>
2018-04-12 14:12:46 -0400

    Add GNU make job/package
    Signed-off-by: Sam Smith <sesmith177@gmail.com>
    Signed-off-by: Sunjay Bhatia <sbhatia@pivotal.io>

---
+++ jobs/make/templates/pre-start.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+$mtx = New-Object System.Threading.Mutex($false, "PathMutex")
+
+if (!$mtx.WaitOne(5000)) {
+  throw "Could not acquire PATH mutex"
+}
+
+$OldPath=(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path
+$AddedFolder="C:\var\vcap\packages\make\bin"
+$NewPath=$OldPath+';'+$AddedFolder
+Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
+
+$mtx.ReleaseMutex()
