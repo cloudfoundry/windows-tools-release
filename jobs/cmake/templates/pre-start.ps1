9bc89b30def5ca1d1b13ac9c437ded1c3666a087
Michael Tang <mitang@pivotal.io>
2018-07-09 14:08:07 -0400

    Fixed bug that caused duplication of paths in PATH for cmake, mingw32, mingw64
    [#158848235](https://www.pivotaltracker.com/story/show/158848235)
    
    Signed-off-by: Jason Smith <jsmith@pivotal.io>

--- jobs/cmake/templates/pre-start.ps1
+++ jobs/cmake/templates/pre-start.ps1
@@ -9,7 +9,10 @@ if (!$mtx.WaitOne(5000)) {
 
 $OldPath=(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path
 $AddedFolder='C:\var\vcap\packages\cmake\bin'
-$NewPath=$OldPath+';'+$AddedFolder
-Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
+
+if (-not $OldPath.Contains($AddedFolder)) {
+  $NewPath=$OldPath+';'+$AddedFolder
+  Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
+}
 
 $mtx.ReleaseMutex()
