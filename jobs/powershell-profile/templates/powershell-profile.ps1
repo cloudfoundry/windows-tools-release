cdfb0d86c9735a5f8f66be27ec2aa70b943d0783
Micah Young <myoung@pivotal.io>
2018-08-21 00:10:29 -0400

    Add job to set $env:PATH from HKLM/Environment
    Generates Powershell profile.ps1. Ported from windows-dev-box.

---
+++ jobs/powershell-profile/templates/powershell-profile.ps1
@@ -0,0 +1,4 @@
+﻿# set PATH to all HKLM Environment
+$env:HKLM_ENV="HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment"
+$env:PATH=(Get-ItemProperty -Path "$env:HKLM_ENV" -Name PATH).Path
+
---
+++ jobs/powershell-profile/templates/pre-start.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+Write-Host 'Set default powershell profile...'
+$profileTemplate="c:\var\vcap\jobs\powershell-profile\bin\powershell-profile.ps1"
+$profileDest="$env:WINDIR\system32\WindowsPowerShell\v1.0\profile.ps1"
+
+Copy-Item -Path "$profileTemplate" -Destination "$profileDest"  -Force
