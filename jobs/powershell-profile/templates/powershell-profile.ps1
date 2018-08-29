6f3b872412f38348f8c81af985a9fc25c1b83d8a
Micah Young <myoung@pivotal.io>
2018-08-29 15:19:19 -0400

    Set GOPATH and GOBIN to data dir

--- jobs/powershell-profile/templates/powershell-profile.ps1
+++ jobs/powershell-profile/templates/powershell-profile.ps1
@@ -2,3 +2,8 @@
 $env:HKLM_ENV="HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment"
 $env:PATH=(Get-ItemProperty -Path "$env:HKLM_ENV" -Name PATH).Path
 
+If (Test-Path "C:\var\vcap\data\golang-windows") {
+  $env:GOPATH="C:\var\vcap\data\golang-windows\go"
+  $env:GOBIN="C:\var\vcap\data\golang-windows\go\bin"
+  $env:PATH+=";$env:GOBIN"
+}
