e1d40e592e574278051368a1a3277cfd2bf244f1
Sam Smith <sesmith177@gmail.com>
2018-10-30 10:03:19 -0400

    Add load-powershell-env job
    Right now, it just will load PATH, but we anticipate it can be updated
    in the future to load more environment variables
    
    Signed-off-by: Amin Jamali <ajamali@pivotal.io>

---
+++ jobs/load-powershell-env/templates/pre-start.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+$profileScript=@'
+$ProgressPreference="SilentlyContinue"
+$hklm_env="HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment"
+
+function ResetEnv {
+  $env:PATH=(Get-ItemProperty -Path "$hklm_env" -Name PATH).Path
+}
+
+ResetEnv
+'@
+
+$profilePath="$ENV:WINDIR\system32\WindowsPowerShell\v1.0\profile.ps1"
+
+Set-Content -Value $profileScript -Path $profilePath -Encoding 'UTF8'
