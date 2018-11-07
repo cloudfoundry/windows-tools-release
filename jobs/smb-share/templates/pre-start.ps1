f181ecccda8a2be6adf806acf3ebf69a5052af16
Amin Jamali <ajamali@pivotal.io>
2018-11-07 17:49:34 -0500

    Force remove the smb-share
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

--- jobs/smb-share/templates/pre-start.ps1
+++ jobs/smb-share/templates/pre-start.ps1
@@ -4,5 +4,5 @@ trap { $host.SetShouldExit(1) }
 $shareDir = "c:\workspace"
 
 New-Item -Type Directory -Path "c:\" -Name "workspace" -Force
-Remove-SmbShare -Name workspace -ErrorAction SilentlyContinue
-New-SmbShare -Name workspace -path $shareDir -FullAccess "Users"
+Remove-SmbShare -Name "workspace" -ErrorAction SilentlyContinue -Force
+New-SmbShare -Name "workspace" -path $shareDir -FullAccess "Users"
