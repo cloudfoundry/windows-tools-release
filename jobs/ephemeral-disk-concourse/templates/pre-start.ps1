6e7fb0be04f94c4b362116566a7b500d156764a6
Yechiel Kalmenson <ykalmenson@pivotal.io>
2018-11-29 13:43:07 -0500

    Set up a few directories required for concourse
    [#161954157]
    
    Signed-off-by: Natalie Arellano <narellano@pivotal.io>

--- jobs/ephemeral-disk-concourse/templates/pre-start.ps1
+++ jobs/ephemeral-disk-concourse/templates/pre-start.ps1
@@ -1,9 +1,14 @@
 $ErrorActionPreference = "Stop";
 trap { $host.SetShouldExit(1) }
 
-if (test-path -Path C:\ProgramData\docker\tmp) {
-    rmdir C:\ProgramData\docker\tmp
+$dockerTmp="C:\ProgramData\docker\tmp"
+If ((Test-Path -Path $dockerTmp) -And ((Get-Item $dockerTmp).LinkType -Ne "SymbolicLink")) {
+    rmdir $dockerTmp
 }
-mkdir C:\var\vcap\data\tmp\docker
-cmd /c mklink /d C:\ProgramData\docker\tmp C:\var\vcap\data\tmp\docker
+mkdir C:\var\vcap\data\tmp\docker -ea 0
+cmd /c mklink /d $dockerTmp C:\var\vcap\data\tmp\docker
+
+# Create Groot Image Store directory
+mkdir C:\var\vcap\data\tmp\groot -ea 0
+
 
