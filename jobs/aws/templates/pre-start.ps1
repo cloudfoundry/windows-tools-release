09d7836b2a73f0495001e3be6219632718007161
Jackson Feeny <jfeeny@pivotal.io>
2019-01-16 15:11:04 -0500

    Add aws cli to windows tools

---
+++ jobs/aws/templates/pre-start.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+$mtx = New-Object System.Threading.Mutex($false, "PathMutex")
+
+if (!$mtx.WaitOne(300000)) {
+  throw "Could not acquire PATH mutex"
+}
+
+$logDir = "c:\var\vcap\sys\log\aws"
+$path=(Get-ChildItem "c:\var\vcap\packages\aws\AWSCLI64*.msi").FullName
+$installDir = "c:\var\vcap\data\aws"
+
+Start-Process -FilePath msiexec -ArgumentList "/i $path SDKFOLDER=$installDir /qn /lxv $logDir\install.log" -Wait -PassThru -NoNewWindow
+
+$mtx.ReleaseMutex()
