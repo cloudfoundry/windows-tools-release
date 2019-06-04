948d8644f5d2c3169639d0debf5250dd56458c43
Sophie Wigmore <swigmore@pivotal.io>
2019-06-04 16:56:51 -0400

    Add vim
    [#165644950]
    
    Signed-off-by: Arjun Sreedharan <asreedharan@pivotal.io>

---
+++ jobs/vim/templates/pre-start.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+$OldPath=(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path
+$AddedFolder="C:\var\vcap\packages\vim\vim_unzipped\vim\vim81"
+
+if (-not $OldPath.Contains($AddedFolder)) {
+  $NewPath=$OldPath+';'+$AddedFolder
+  Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
+}
+
+cp bin/_vimrc $env:HOME/_vimrc
