ec5a4112702f5202e5a8a54b8626dc60b429b3ab
Tal Usvyatsky <tusvyatsky@pivotal.io>
2019-09-03 14:44:13 -0400

    Mount point for go path is not version
    - At least for BOSH Windows
    - Maintained version GOPATH approach in case of other teams using different mount points
    
    Co-authored-by: Simon Jones <sijones@pivotal.io>

--- jobs/powershell-profile/templates/powershell-profile.ps1
+++ jobs/powershell-profile/templates/powershell-profile.ps1
@@ -2,6 +2,12 @@
 $env:HKLM_ENV="HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment"
 $env:PATH=(Get-ItemProperty -Path "$env:HKLM_ENV" -Name PATH).Path
 
+If (Test-Path "C:\var\vcap\data\golang-windows") {
+  $env:GOPATH="C:\var\vcap\data\golang-windows\go"
+  $env:GOBIN="C:\var\vcap\data\golang-windows\go\bin"
+  $env:PATH+=";$env:GOBIN"
+}
+
 If (Test-Path "C:\var\vcap\data\golang-1.12-windows") {
   $env:GOPATH="C:\var\vcap\data\golang-1.12-windows\go"
   $env:GOBIN="C:\var\vcap\data\golang-1.12-windows\go\bin"
