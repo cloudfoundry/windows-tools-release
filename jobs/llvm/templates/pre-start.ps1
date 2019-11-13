73635f90b5256edc42f25d160d04758589bcc445
Yechiel Kalmenson <ykalmenson@pivotal.io>
2019-11-13 12:23:56 -0500

    add llvm bosh job
    Signed-off-by: William A Rowe Jr <wrowe@pivotal.io>

---
+++ jobs/llvm/templates/pre-start.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+$mtx = New-Object System.Threading.Mutex($false, "PathMutex")
+
+if (!$mtx.WaitOne(300000)) {
+  throw "Could not acquire PATH mutex"
+}
+
+$installDir = "c:\var\vcap\data\llvm"
+Start-Process -FilePath c:\var\vcap\packages\llvm\LLVM-win64.exe -ArgumentList "/S /D=$installDir" -Wait -PassThru -NoNewWindow
+
+$mtx.ReleaseMutex()
--- jobs/vs_buildtools/templates/pre-start.ps1
+++ jobs/vs_buildtools/templates/pre-start.ps1
@@ -2,7 +2,7 @@
 trap { $host.SetShouldExit(1) }
 
 $installDir = "c:\var\vcap\data\VSBuildTools\2019"
-cmd.exe /s /c "c:\var\vcap\packages\vs_buildtools\vs_buildtools.exe --installPath $installDir --passive --wait --norestart --nocache --add Microsoft.VisualStudio.Component.VC.CoreBuildTools --add Microsoft.VisualStudio.Component.VC.Redist.14.Latest --add Microsoft.VisualStudio.Component.VC.Llvm.Clang --add Microsoft.VisualStudio.Component.VC.Llvm.ClangToolset --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.Windows10SDK"
+cmd.exe /s /c "c:\var\vcap\packages\vs_buildtools\vs_buildtools.exe --installPath $installDir --passive --wait --norestart --nocache --add Microsoft.VisualStudio.Component.VC.CoreBuildTools --add Microsoft.VisualStudio.Component.VC.Redist.14.Latest --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.Windows10SDK"
 
 if ($LASTEXITCODE -ne 0) {
 	echo "VS Build Tools install failed: $LASTEXITCODE"
