f5af4b429fb40975bf717b2662ea89bb0e22b667
Genevieve LEsperance <glesperance@pivotal.io>
2019-11-25 11:26:49 -0500

    Add golang-1-windows job
    Signed-off-by: Arjun Sreedharan <asreedharan@pivotal.io>

---
+++ jobs/golang-1-windows/templates/pre-start.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+$mtx = New-Object System.Threading.Mutex($false, "PathMutex")
+
+if (!$mtx.WaitOne(300000)) {
+  throw "Could not acquire PATH mutex"
+}
+
+$GoRoot='C:\var\vcap\packages\golang-1-windows\go'
+
+$OldPath=(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path
+$AddedFolder="$GoRoot\bin"
+
+if (-not $OldPath.Contains($AddedFolder)) {
+  $NewPath=$OldPath+';'+$AddedFolder
+  Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
+}
+
+Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name GOROOT -Value $GoRoot
+
+$mtx.ReleaseMutex()
