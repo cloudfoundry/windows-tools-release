5b912680a9859cb88b219e4c882c861e2bf5f0e7
William A Rowe Jr <wrowe@pivotal.io>
2019-12-04 15:04:45 -0500

    Push to latest msys2 release
    - cannot use bosh 'tar' for .xz, it would require the xz.exe binary
    - shift updating the msys2 release to when we rebuild the release
    - stop creating a conemu (conhost) window during compilation (headless)
    - invoke pacman from true path to avoid smashing c:\var\vcap\data symlink
    - no longer change components during deployment
    
    Signed-off-by: Yechiel Kalmenson <ykalmenson@pivotal.io>
    Signed-off-by: Anthony Emengo <aemengo@pivotal.io>

--- jobs/msys2/templates/pre-start.ps1
+++ jobs/msys2/templates/pre-start.ps1
@@ -15,20 +15,4 @@ if (-not $OldPath.Contains($msys2UsrBin)) {
   Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
 }
 
-#https://github.com/msys2/msys2/wiki/MSYS2-installation
-Write-Host "Starting initialization via msys2_shell.cmd"
-Start-Process "c:\var\vcap\packages\msys2\msys2_shell.cmd" -Wait -ArgumentList '-c', exit
-
-Write-Host "Repeating system update until there are no more updates or max 5 iterations"
-$ErrorActionPreference = 'Continue'
-Set-Alias bash "$msys2UsrBin\bash.exe"
-while (!$done) {
-    Write-Host "`n================= SYSTEM UPDATE $((++$i)) =================`n"
-    bash -lc 'pacman --noconfirm -Syuu | tee /update.log'
-    $done = (Get-Content "c:\var\vcap\packages\msys2\update.log") -match 'there is nothing to do' | Measure-Object | ForEach-Object { $_.Count -eq 2 }
-    $done = $done -or ($i -ge 5)
-}
-bash -lc 'pacman --noconfirm -S patch'
-Remove-Item "c:\var\vcap\packages\msys2\update.log" -ea 0
-
 $mtx.ReleaseMutex()
