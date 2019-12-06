42c8ddb0c56aa07881e01dfe327d2091aa04593d
Daniel Basner <dbasner@pivotal.io>
2019-12-06 15:14:37 -0500

    Remove default Pester module if present before upgrading Pester
    [#168997593](https://www.pivotaltracker.com/story/show/168997593)
     Extract BOSH.PSModules out to its own repo
    
    Co-authored-by: George Gelashvili <ggelashvili+cf@pivotal.io>

--- jobs/pester/templates/pre-start.ps1
+++ jobs/pester/templates/pre-start.ps1
@@ -1,12 +1,17 @@
 $ErrorActionPreference = "Stop";
 trap { $host.SetShouldExit(1) }
 
-Write-Host "Installing pester"
+Write-Log "Installing Pester"
+$powershellModulePath="$env:PROGRAMFILES\WindowsPowerShell\Modules"
+$pesterModulePath="$powershellModulePath\Pester"
 
-$pesterModulePath="$env:PROGRAMFILES\WindowsPowerShell\Modules\Pester"
-
-Remove-Item -Recurse -Force "$pesterModulePath"
-Copy-Item -Recurse "C:\var\vcap\packages\pester\Pester" "$pesterModulePath"
-Write-Host "Installed pester powershell module"
-
+if (Test-Path $pesterModulePath) {
+    Write-Log "Removing existing default Pester module so it can be upgraded"
+    takeown /F $pesterModulePath /A /R
+    icacls $pesterModulePath /reset
+    icacls $pesterModulePath /grant Administrators:'F' /inheritance:d /T
+    Remove-Item -Recurse -ErrorAction Ignore "$pesterModulePath" -Confirm:$false
+}
+Copy-Item -Recurse "C:\var\vcap\packages\pester\Pester" "$powershellModulePath\" -Verbose
 Import-Module Pester -Force
+Write-Log "Pester Installed"
