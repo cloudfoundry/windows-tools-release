7679c757273309114f6a82db12344add3c2100e0
William A Rowe Jr <wrowe@pivotal.io>
2020-03-27 13:02:28 -0400

    Improve msys2 packaging and startup
    - replace usage of 7zip with git package provided tar to un-xz msys2
      file
    - modify installed packages to include all from the `compression`
      package
    - clean up resulting msys2 package manager
    - move msys2 bin to the end of the path
    - link msys2 tmp to C:\tmp so files are not deposited under
     /var/vcap/packages/msys2/tmp

--- jobs/msys2/templates/pre-start.ps1
+++ jobs/msys2/templates/pre-start.ps1
@@ -11,8 +11,13 @@ $OldPath=(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentCon
 $msys2UsrBin="c:\var\vcap\packages\msys2\usr\bin"
 
 if (-not $OldPath.Contains($msys2UsrBin)) {
-  $NewPath=$msys2UsrBin+';'+$OldPath
+  # add msys2 to the end of the path so binaries like link.exe are found sooner (we likely don't want the one from msys2)
+  $NewPath=$OldPath+';'+$msys2UsrBin
   Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
 }
 
 $mtx.ReleaseMutex()
+
+# link the msys2 tmp directory to a system wide tmp so generated "/tmp" paths work correctly
+mkdir -force C:\tmp
+cmd.exe /c mklink C:\var\vcap\packages\msys2\tmp C:\tmp
