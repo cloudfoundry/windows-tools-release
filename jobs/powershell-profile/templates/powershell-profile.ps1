45a13c4f02a2a5fec91bfb10a6428e176711e222
Anthony Emengo <anthonyemengojr@gmail.com>
2020-04-02 11:58:20 -0400

    Remove all references to golang 1.13
    * Prefer golang-1-windows job/packages because that is systematically
      updated

--- jobs/powershell-profile/templates/powershell-profile.ps1
+++ jobs/powershell-profile/templates/powershell-profile.ps1
@@ -2,14 +2,7 @@
 $env:HKLM_ENV="HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment"
 $env:PATH=(Get-ItemProperty -Path "$env:HKLM_ENV" -Name PATH).Path
 
-If (Test-Path "C:\var\vcap\data\golang-windows") {
-  $env:GOPATH="C:\var\vcap\data\golang-windows\go"
-  $env:GOBIN="C:\var\vcap\data\golang-windows\go\bin"
-  $env:PATH+=";$env:GOBIN"
-}
 
-If (Test-Path "C:\var\vcap\data\golang-1.13-windows") {
-  $env:GOPATH="C:\var\vcap\data\golang-1.13-windows\go"
-  $env:GOBIN="C:\var\vcap\data\golang-1.13-windows\go\bin"
-  $env:PATH+=";$env:GOBIN"
-}
+$env:GOPATH="C:\var\vcap\data\golang-1-windows\go"
+$env:GOBIN="C:\var\vcap\data\golang-1-windows\go\bin"
+$env:PATH+=";$env:GOBIN"
